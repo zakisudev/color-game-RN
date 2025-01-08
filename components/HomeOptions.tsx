8019f772fc132289cb232ce96dbbe9e0d0465e8d
zakisudev <zakisudev@gmail.com>
2025-01-08 15:45:31 +0530

    Remove background color from HomeOptions component styles

--- components/HomeOptions.tsx
+++ components/HomeOptions.tsx
@@ -74,7 +74,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		flexDirection: "column",
-		backgroundColor: "gray",
 		justifyContent: "space-around",
 		alignItems: 'center',
 		width: '100%',
